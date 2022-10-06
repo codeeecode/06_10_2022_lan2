392bfa10b395d04817d126576d0dfdcd3aa446a4
minh <minhnvgch200213@fpt.edu.vn>
2022-10-06 17:38:54 +0700

    first commit

--- lab9/app.js
+++ lab9/app.js
@@ -14,7 +14,7 @@ var app = express();
 //cấu hình mongoose (database)
 var mongoose = require("mongoose");
 var db =
-  "mongodb+srv://longndt:xdHB48B3Fv*%40TPa@cluster0.i5kings.mongodb.net/greenwich";
+    "mongodb+srv://codecode12345:123456789m@cluster0.ik5tvr8.mongodb.net/greenwich"
 mongoose.connect(db, { useNewUrlParser: true });
 
 //cấu hình body-parser (form input)
@@ -37,25 +37,25 @@ app.use("/mobile", mobileRouter);
 app.use("/laptop", laptopRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log("Server is running http://localhost:3000");
+    console.log("Server is running http://localhost:8000");
 });
 
 module.exports = app;
--- lab9/models/MobileSchema.js
+++ lab9/models/MobileSchema.js
@@ -3,10 +3,10 @@ var schema = mongoose.Schema;
 var model = mongoose.model;
 
 var MobileSchema = schema({
-  name: String,
-  brand: String,
-  image: String,
-  price: Number,
+    name: String,
+    brand: String,
+    image: String,
+    price: Number,
 });
 
 var MobileModel = model("Điện thoại", MobileSchema, "mobile");
